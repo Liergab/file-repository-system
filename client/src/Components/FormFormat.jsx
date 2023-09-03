9cb7f3b9d3c09430b4c91c05e620d53b216289ba
BRYAN <bryangabrielberja25@gmail.com>
2023-09-03 14:14:36 +0800

    'darkmode_of_form_formar'

--- client/src/Components/FormFormat.jsx
+++ client/src/Components/FormFormat.jsx
@@ -4,8 +4,8 @@
 const FormFormat = ({children}) => {
   return (
     <div className=" h-[calc(100vh-80px)] flex items-center place-content-center dark:text-gray-100 dark:bg-blue-gray-900 duration-100  ">
-     <div className="h-auto md:w-3/12 w-4/5 bg-white rounded-md bg-clip-padding backdrop-filter 
-                            backdrop-blur-lg bg-opacity-50 border border-gray-100 shadow-md px-6 py-4">
+     <div className="h-auto md:w-3/12 w-4/5  dark:bg-white  rounded-md 
+                            border border-gray-100 shadow-md px-6 py-4">
         {children}
      </div>
      
